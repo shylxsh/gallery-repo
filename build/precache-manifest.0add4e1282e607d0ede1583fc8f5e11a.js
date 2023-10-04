a4545484f508b1b932b9046162c0ec97f2fb2628
Shylesh Shankar <106320494+shylxsh@users.noreply.github.com>
2023-10-04 15:10:07 +0530

    Add files via upload

---
+++ build/precache-manifest.0add4e1282e607d0ede1583fc8f5e11a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f33d8e083bf63e707020c4c62cae6ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3889862ab903d42a92d",
+    "url": "/static/css/main.e3bd3a6e.chunk.css"
+  },
+  {
+    "revision": "09cc9ce0a99b689b3100",
+    "url": "/static/js/2.944440e5.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/static/js/2.944440e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3889862ab903d42a92d",
+    "url": "/static/js/main.0a1c143f.chunk.js"
+  },
+  {
+    "revision": "f35af6f7ccead2fe5dbb",
+    "url": "/static/js/runtime-main.4422d9b2.js"
+  }
+]);
